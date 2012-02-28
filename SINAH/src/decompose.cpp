09fa0bd837f2d1dcb6ee7b6da7d25731728e3123
James Lindsay <james@jamesrlindsay.com>
2012-02-27 22:26:25 -0500

    updated makefile

--- SINAH/src/decompose.cpp
+++ SINAH/src/decompose.cpp
@@ -1,14 +1,15 @@
 /*
- * SINAH.cpp
+ * decompose.cpp
  *
  *  Created on: Feb 27, 2012
  *      Author: jlindsay
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("Hello World!!!");
+	puts("CHEESY BAGEL");
 	return EXIT_SUCCESS;
 }
