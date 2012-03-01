996f30a7bf61390117b6e669c0754f8a10bc2e00
James Lindsay <james@jamesrlindsay.com>
2012-03-01 02:14:20 -0500

    only 2-decompose left

--- SINAH/src/decompose.cpp
+++ SINAH/src/decompose.cpp
@@ -17,12 +17,20 @@ using google::sparse_hash_set;
 // declare space for debug text.
 char * DEBUGTXT = new char[5012];
 
+// create global node index.
+int GLOBAL_IDX = 0;
+
 int main(int argc, char* argv[]) {
 
 	// arguments.
 	const char * node_file = argv[1];
 	const char * bundle_file = argv[2];
 	unsigned int cutoff = atoi(argv[3]);
+
+	// sanity check.
+	if( cutoff < 5 ){
+		cutoff = 5;
+	}
 
 	// load graph information.
 	WRITE_OUT("loading graph information\n");
@@ -45,9 +53,8 @@ int main(int argc, char* argv[]) {
 	// execute zero decomposition.
 	zero_decomp(BG, DG);
 
-	// verify zero decomposition.
-	//verify_connected(np, bp, DG);
-
+	// verify decomposition.
+	verify_connected(np, bp, DG);
 
 	// digg deeper for 1 decomposition.
 	DecompGraph::vertex_iterator vertexIt, vertexEnd;
@@ -72,10 +79,10 @@ int main(int argc, char* argv[]) {
 		// execute one decomposition.
 		one_decomp(subg, DG, vertexID);
 
-
-		// verify one decomposition.
-		//verify_connected(np, bp, DG);
 	}
+
+	// verify decomposition.
+	verify_connected(np, bp, DG);
 
 	// digg deeper for 2 decomposition.
 	boost::tie(vertexIt, vertexEnd) = vertices(DG);
@@ -99,12 +106,13 @@ int main(int argc, char* argv[]) {
 		// execute one decomposition.
 		two_decomp(subg, DG, vertexID);
 
-		// verify one decomposition.
-		//verify_connected(np, bp, DG);
 	}
 
-	// write out decomposition to disk.
+	// verify decomposition.
+	verify_connected(np, bp, DG);
 
+	// write out decomposition to disk.
+	WRITE_OUT("completed succesfully.\n");
 	return EXIT_SUCCESS;
 }
 
@@ -118,8 +126,8 @@ void zero_decomp(BundleGraph BG, DecompGraph & DG){
 	const Graph G = BG.G;
 
 	// find connected components.
-	NodeArray<int> zero_comps(G);
-	int cnt = connectedComponents(G, zero_comps);
+	NodeArray<int> comps(G);
+	int cnt = connectedComponents(G, comps);
 
 	// add nodes to DG.
 	sparse_hash_map<int, VertexID> nlookup;
@@ -133,12 +141,14 @@ void zero_decomp(BundleGraph BG, DecompGraph & DG){
 		DG[vID].idx = i;
 		DG[vID].stage = 0;
 		DG[vID].set.clear();
+		DG[vID].global = GLOBAL_IDX;
+		GLOBAL_IDX++;
 	}
 
 	// populate node set.
 	node n;
 	forall_nodes(n, G){
-		DG[nlookup[zero_comps[n]]].set.insert(BG.n2idx[n]);
+		DG[nlookup[comps[n]]].set.insert(BG.n2idx[n]);
 	}
 
 }
@@ -152,15 +162,19 @@ void one_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 	// save graph.
 	Graph G = BG.G;
 
+	// sanity check.
+	if( isConnected(G) == false ){
+		WRITE_ERR("not connected: call jwow!\n");
+		exit(1);
+	}
+
 	// declare vars.
 	node n, n1, n2;
 	long id1, id2, cidx;
 
-	// build BC tree and auxillary graph.
-	BCTree BC(G);
-	const Graph &BC_T = BC.bcTree();
-	const Graph &BC_G = BC.auxiliaryGraph();
-	int cnt = BC.numberOfBComps();
+	// find connected components.
+	EdgeArray<int> comps(G);
+	int cnt = biconnectedComponents(G, comps);
 
 	// add nodes to DG.
 	sparse_hash_map<int, VertexID> nlookup;
@@ -174,53 +188,35 @@ void one_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 		DG[vID].idx = i;
 		DG[vID].stage = 1;
 		DG[vID].set.clear();
-
+		DG[vID].global = GLOBAL_IDX;
+		GLOBAL_IDX++;
 	}
 
-	// create node lookup node array.
-	NodeArray<node> node_map(BC_G);
+	// populate node set.
+	edge e;
+	forall_edges(e, G){
 
-	// assign each Bnode to a component.
-	NodeArray<int> bctidx(BC_T);
-	cidx = 0;
-	forall_nodes(n, BC_T){
-		if(BC.typeOfBNode(n) == BC.BComp){
+		// get nodes.
+		n1 = e->source();
+		n2 = e->target();
 
-			// map node to component index.
-			bctidx[n] = cidx;
-
-			// retrieve edges from b components.
-			const SList<edge> x = BC.hEdges(n);
-
-			// add nodes to this component via its edges.
-			for(SListConstIterator<edge> it = x.begin(); it!=x.end(); it++){
-
-				// simplify.
-				n1 = (*it)->source();
-				n2 = (*it)->target();
-				id1 = BG.n2idx[BC.original(n1)];
-				id2 = BG.n2idx[BC.original(n2)];
-
-				// add nodes to set.
-				DG[nlookup[cidx]].set.insert(id1);
-				DG[nlookup[cidx]].set.insert(id2);
-			}
-
-			// increment pointer.
-			cidx++;
-		}
+		// add to graph.
+		DG[nlookup[comps[e]]].set.insert(BG.n2idx[n1]);
+		DG[nlookup[comps[e]]].set.insert(BG.n2idx[n2]);
 	}
 
-	// add root.
-	EdgeID edge;
-	bool ok;
-	boost::tie(edge, ok) = boost::add_edge(parent, nlookup[0], DG);
-	if(ok == false){
-		WRITE_ERR("problem adding edge.\n");
-		exit(1);
+
+	// add nodes to TGraph
+	TGraph TG;
+	sparse_hash_map<int, VertexID> tlookup;
+	for(int i=0; i<cnt; i++){
+
+		// add node.
+		TVertexID vID = boost::add_vertex(TG);
+		tlookup[i] = vID;
 	}
 
-	// verify 1-cuts.
+	// add edges to TGraph
 	NodeSet::iterator it;
 	vector<int> intersection;
 	for(int i=0; i<cnt; i++){
@@ -253,17 +249,53 @@ void one_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 			// save intersection.
 			if( intersection.size() == 1 ){
 
-				// insert into vector.
-				EdgeID edge;
+				// get TGraph nodes.
+				TVertexID t1 = tlookup[i];
+				TVertexID t2 = tlookup[j];
+
+				// add edge.
+				TEdgeID edge;
 				bool ok;
-				boost::tie(edge, ok) = boost::add_edge(vertexID1, vertexID2, DG);
+				boost::tie(edge, ok) = boost::add_edge(t2, t1, TG);
 				if(ok == false){
 					WRITE_ERR("problem adding edge.\n");
 					exit(1);
 				}
-				DG[edge].cuts.push_back(intersection.front());
+				TG[edge].cuts.push_back(intersection.front());
 			}
 		}
+	}
+
+	// DFS this graph to build the decomposition tree.
+	MyVisitor vis;
+	boost::depth_first_search(TG, boost::visitor(vis));
+
+	// apply the decomposition edges to the actual graph.
+	for(vector<Etmp>::iterator it=vis.elist.begin(); it!=vis.elist.end(); it++){
+
+		// grab nodes.
+		VertexID p = nlookup[it->source];
+		VertexID q = nlookup[it->target];
+
+		// add edges.
+		EdgeID edge;
+		bool ok;
+		boost::tie(edge, ok) = boost::add_edge(p, q, DG);
+		if(ok == false){
+			WRITE_ERR("problem adding edge.\n");
+			exit(1);
+		}
+		DG[edge].cuts = it->cuts;
+	}
+
+	// add root edge.
+	TVertexID r = *vertices(TG).first;
+	EdgeID edge;
+	bool ok;
+	boost::tie(edge, ok) = boost::add_edge(parent, nlookup[TG[r].idx], DG);
+	if(ok == false){
+		WRITE_ERR("problem adding edge.\n");
+		exit(1);
 	}
 }
 
@@ -276,14 +308,85 @@ void two_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 	// save graph.
 	Graph G = BG.G;
 
+	// sanitfy check.
+	if( isBiconnected(G) == false ){
+		WRITE_ERR("not biconnected: call snooki!\n");
+		exit(1);
+	}
+
 	// declare vars.
 	node n, p, q;
-	long idx, id1;
+	long idx, id1, id2;
 
 	// get spqr structure.
 	StaticSPQRTree SPQR(G);
 	const Graph &SPQR_T = SPQR.tree();
-	int cnt = SPQR_T.numberOfNodes();
+
+	// identify cuts.
+	vector<pair<int, int> > cuts;
+	forall_nodes(n, SPQR_T){
+
+		// get skeleton and its graph.
+		const Skeleton &sk = SPQR.skeleton(n);
+		const Graph &g = sk.getGraph();
+
+		// find virtual edges.
+		edge e;
+		forall_edges(e, g){
+			if( sk.isVirtual(e) == true ){
+				// get cuts.
+				p = sk.original(e->source());
+				q = sk.original(e->target());
+				id1 = BG.n2idx[p];
+				id2 = BG.n2idx[q];
+
+				// add cuts to vector.
+				if( id1 < id2 ){
+					cuts.push_back(pair<int,int>(id1, id2));
+				} else {
+					cuts.push_back(pair<int,int>(id2, id1));
+				}
+			}
+		}
+	}
+
+	// build list of neighbors for each cut.
+	sparse_hash_map<int, vector<int> > neighbors;
+	for(hsize_t i=0; i<cuts.size(); i++){
+
+		// simplify.
+		id1 = cuts[i].first;
+		id2 = cuts[i].second;
+
+		// check if need neighbor search.
+		if( neighbors.find(id1) == neighbors.end() ){
+			// look for neighbors.
+			neighbors[id1] = vector<int>();
+			adjEntry adj;
+			forall_adj(adj, BG.idx2n[id1]){
+				neighbors[id1].push_back(BG.n2idx[adj->theNode()]);
+			}
+		}
+
+		// check if need neighbor search.
+		if( neighbors.find(id2) == neighbors.end() ){
+			// look for neighbors.
+			neighbors[id2] = vector<int>();
+			adjEntry adj;
+			forall_adj(adj, BG.idx2n[id2]){
+				neighbors[id2].push_back(BG.n2idx[adj->theNode()]);
+			}
+		}
+	}
+
+	// remove all cuts from graph.
+	for(sparse_hash_map<int, vector<int> >::iterator it=neighbors.begin(); it!=neighbors.end(); it++){
+		G.delNode(BG.idx2n[it->first]);
+	}
+
+	// identify connected components.
+	NodeArray<int> comps(G);
+	int cnt = connectedComponents(G, comps);
 
 	// add nodes to DG.
 	sparse_hash_map<int, VertexID> nlookup;
@@ -297,46 +400,46 @@ void two_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 		DG[vID].idx = i;
 		DG[vID].stage = 2;
 		DG[vID].set.clear();
-
+		DG[vID].global = GLOBAL_IDX;
+		GLOBAL_IDX++;
 	}
 
-	// add nodes to sets.
-	idx = 0;
-	forall_nodes(n, SPQR_T){
-
-		// check for root.
-		if( SPQR.rootNode() == n ){
-			EdgeID edge;
-			bool ok;
-			boost::tie(edge, ok) = boost::add_edge(parent, nlookup[idx], DG);
-			if(ok == false){
-				WRITE_ERR("problem adding edge.\n");
-				exit(1);
-			}
-		}
-
-		// get skeleton and its graph.
-		const Skeleton &sk = SPQR.skeleton(n);
-		const Graph &g = sk.getGraph();
-
-		// verify skeleton is connected.
-		if( isConnected(g) == false ){
-			printf("error skeleton is not connected.\n");
-			exit(1);
-		}
-
-		// add nodes to set.
-		forall_nodes(p, g){
-			q = sk.original(p);
-			id1 = BG.n2idx[q];
-			DG[nlookup[idx]].set.insert(id1);
-		}
-
-		// increment index.
-		idx++;
+	// populate node set with non-cut.
+	sparse_hash_map<int, VertexID> xlookup;
+	forall_nodes(n, G){
+		DG[nlookup[comps[n]]].set.insert(BG.n2idx[n]);
+		xlookup[BG.n2idx[n]] = nlookup[comps[n]];
 	}
 
-	// verify 2-cuts.
+	// add the cut nodes.
+	for(sparse_hash_map<int, vector<int> >::iterator it=neighbors.begin(); it!=neighbors.end(); it++){
+
+		// get cut node.
+		id1 = it->first;
+
+		// loop over neighbors.
+		for(vector<int>::iterator it2=it->second.begin(); it2!=it->second.end(); it2++){
+
+			// get placed node.
+			id2 = *it2;
+
+			// add cut node to placed nodes component.
+			DG[xlookup[id2]].set.insert(id1);
+
+		}
+	}
+
+	// add nodes to TGraph
+	TGraph TG;
+	sparse_hash_map<int, VertexID> tlookup;
+	for(int i=0; i<cnt; i++){
+
+		// add node.
+		TVertexID vID = boost::add_vertex(TG);
+		tlookup[i] = vID;
+	}
+
+	// add edges to TGraph
 	NodeSet::iterator it;
 	vector<int> intersection;
 	for(int i=0; i<cnt; i++){
@@ -361,32 +464,69 @@ void two_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent){
 			}
 
 			// sanity check.
-			if( intersection.size() > 2  ){
+			if( intersection.size() > 2 ){
 				WRITE_ERR("error in two decomposition\n");
-				for(vector<int>::iterator it2 = intersection.begin(); it2!= intersection.end(); it2++){
-					cout << *it2 << endl;
-				}
+				exit(1);
+			}
+
+			// sanity check.
+			if( intersection.size() == 1 ){
+				WRITE_ERR("error in two decomposition2\n");
 				exit(1);
 			}
 
 			// save intersection.
 			if( intersection.size() == 2 ){
 
-				// insert into vector.
-				EdgeID edge;
+				// get TGraph nodes.
+				TVertexID t1 = tlookup[i];
+				TVertexID t2 = tlookup[j];
+
+				// add edge.
+				TEdgeID edge;
 				bool ok;
-				boost::tie(edge, ok) = boost::add_edge(vertexID1, vertexID2, DG);
+				boost::tie(edge, ok) = boost::add_edge(t2, t1, TG);
 				if(ok == false){
 					WRITE_ERR("problem adding edge.\n");
 					exit(1);
 				}
-				DG[edge].cuts.push_back(intersection.front());
-				DG[edge].cuts.push_back(intersection.back());
-
+				TG[edge].cuts.push_back(intersection.front());
+				TG[edge].cuts.push_back(intersection.back());
 			}
 		}
 	}
 
+	// DFS this graph to build the decomposition tree.
+	MyVisitor vis;
+	boost::depth_first_search(TG, boost::visitor(vis));
+
+	// apply the decomposition edges to the actual graph.
+	for(vector<Etmp>::iterator it=vis.elist.begin(); it!=vis.elist.end(); it++){
+
+		// grab nodes.
+		VertexID p = nlookup[it->source];
+		VertexID q = nlookup[it->target];
+
+		// add edges.
+		EdgeID edge;
+		bool ok;
+		boost::tie(edge, ok) = boost::add_edge(p, q, DG);
+		if(ok == false){
+			WRITE_ERR("problem adding edge.\n");
+			exit(1);
+		}
+		DG[edge].cuts = it->cuts;
+	}
+
+	// add root edge.
+	TVertexID r = *vertices(TG).first;
+	EdgeID edge;
+	bool ok;
+	boost::tie(edge, ok) = boost::add_edge(parent, nlookup[TG[r].idx], DG);
+	if(ok == false){
+		WRITE_ERR("problem adding edge.\n");
+		exit(1);
+	}
 }
 
 // verify the decomposition yields connected components.
@@ -395,6 +535,7 @@ void verify_connected(NodePair np, BundlePair bp, DecompGraph DG){
 	// announce.
 	WRITE_OUT("verifying...\n");
 
+	int type;
 	DecompGraph::vertex_iterator vertexIt, vertexEnd;
 	boost::tie(vertexIt, vertexEnd) = vertices(DG);
 	for (; vertexIt != vertexEnd; ++vertexIt){
@@ -404,6 +545,7 @@ void verify_connected(NodePair np, BundlePair bp, DecompGraph DG){
 
 	    // get the actual vertex.
 	    DGVertex & vertex = DG[vertexID];
+	    type = vertex.stage;
 
 	    // check if empty.
 	    if( vertex.set.size() == 0 ){
@@ -418,6 +560,20 @@ void verify_connected(NodePair np, BundlePair bp, DecompGraph DG){
 		if( isConnected(subg.G) == false ){
 			WRITE_ERR("error: is not connected.\n");
 			exit(1);
+		}
+
+		// check type.
+		if( type > 0 ){
+			if( isBiconnected(subg.G) == false ){
+				WRITE_ERR("error: is not connected.\n");
+				exit(1);
+			}
+		}
+		if( type > 1 ){
+			if( isTriconnected(subg.G) == false ){
+				WRITE_ERR("error: is not connected.\n");
+				exit(1);
+			}
 		}
 	}
 }
--- SINAH/src/decompose.h
+++ SINAH/src/decompose.h
@@ -18,6 +18,8 @@
 // boost BGL
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/visitors.hpp>
+#include <boost/graph/depth_first_search.hpp>
+
 
 // bundle graph.
 #include "BundleGraph.h"
@@ -28,7 +30,12 @@
 struct DGVertex {
     int idx;
     int stage;
+    int global;
 	google::sparse_hash_set<int> set;
+};
+
+struct TVertex {
+    int idx;
 };
 
 struct DGEdge {
@@ -43,8 +50,19 @@ typedef boost::adjacency_list<  // adjacency_list is a template depending on :
     DGEdge                        //  The type that describes an Edge
 > DecompGraph;
 
+typedef boost::adjacency_list<  // adjacency_list is a template depending on :
+    boost::listS,               //  The container used for egdes : here, std::list.
+    boost::vecS,                //  The container used for vertices: here, std::vector.
+    boost::undirectedS,           //  directed or undirected edges ?.
+    TVertex,                     //  The type that describes a Vertex.
+    DGEdge                        //  The type that describes an Edge
+> TGraph;
+
 typedef DecompGraph::vertex_descriptor VertexID;
 typedef DecompGraph::edge_descriptor   EdgeID;
+
+typedef TGraph::vertex_descriptor TVertexID;
+typedef TGraph::edge_descriptor   TEdgeID;
 
 // decomposition functions.
 void zero_decomp(BundleGraph BG, DecompGraph & DG);
@@ -53,5 +71,34 @@ void two_decomp(BundleGraph BG, DecompGraph & DG, VertexID parent);
 
 // ancillary functions.
 void verify_connected(NodePair np, BundlePair bp, DecompGraph decomp);
+
+// visitor for DFS.
+struct Etmp {
+	int source;
+	int target;
+    std::vector<int> cuts;
+};
+class MyVisitor : public boost::default_dfs_visitor {
+public:
+	// track directed edges in tree.
+	std::vector<Etmp> elist;
+	void tree_edge(TEdgeID e, const TGraph& g) {
+
+		// get parent and child.
+		TVertexID p = source(e, g);
+		TVertexID q = target(e, g);
+
+		// build entry.
+		Etmp tmp;
+		tmp.source = g[p].idx;
+		tmp.target = g[q].idx;
+		tmp.cuts = g[e].cuts;
+
+		// add it.
+		elist.push_back(tmp);
+		return;
+	}
+};
+
 
 #endif /* DECOMPOSE_H_ */
